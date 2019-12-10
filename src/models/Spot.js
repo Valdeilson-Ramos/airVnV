9544d8949039f78d9788183d58a88abffd148db0
valdeilson <vautronica@gmail.com>
2019-12-09 23:52:57 -0300

    criando rotas para vizualizar imagens

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -1,14 +1,24 @@
 const mongoose = require("mongoose");
 
-const SpotSchema = new mongoose.Schema({
-  thumbnail: String, //nome da imagem da empresa
-  company: String, //empresa
-  price: Number, //preços
-  techs: [String], //teconologias que a empresa trabalha
-  user: {
-    type: mongoose.Schema.Types.ObjectId, //reprentando um relacionamento, ou seja o usuário que criou
-    ref: "User" //(referencia para o model usuario)
+const SpotSchema = new mongoose.Schema(
+  {
+    thumbnail: String, //nome da imagem da empresa
+    company: String, //empresa
+    price: Number, //preços
+    techs: [String], //teconologias que a empresa trabalha
+    user: {
+      type: mongoose.Schema.Types.ObjectId, //reprentando um relacionamento, ou seja o usuário que criou
+      ref: "User" //(referencia para o model usuario)
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
+);
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model("Spot", SpotSchema);
